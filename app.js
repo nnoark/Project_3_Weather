c849e6e4237a11a4681d1a5a4cacec5868d0119d
AnthonyStevens1 <astevens600@gmail.com>
2021-11-16 20:53:01 -0500

    cleanup

--- app.js
+++
@@ -1,5 +0,0 @@
-// connect to MongoDB
-const {MongoClient} = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
-await client.connect();
