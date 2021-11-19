ef65a447eb923ccbc85309c1c5b8a0503ed1e57f
nnoark <nnoark@gmail.com>
2021-11-19 16:02:19 -0500

    code for markers added, needs tweaked

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -18,16 +18,39 @@ d3.json(json).then(function(data) {
 
   for (var i = 0; i < data.metadata.length; i++) {
     var location = [data.metadata[i].Lat,data.metadata[i].Lng]
+    var city = [data.metadata[i]["City"]]
+    var maxtemp = [data.metadata[i]["Max Temp"]]
+    var humidity = [data.metadata[i]["Humidity"]]
+    var wind = [data.metadata[i]["Wind Speed"]]
+
   
 
     if (location) {
-      heatArray.push([location[0],location[1]]);
+      heatArray.push([location[0],location[1],maxtemp]);
     }
   }
 
-  var heat = L.heatLayer(heatArray, {
-    radius: 80,
-    blur: 35
-  }).addTo(myMap);
 
+    var heat = L.heatLayer(heatArray, {
+      max: Math.max(location[2]),
+      radius: 80,
+      blur: 35,
+        }).addTo(myMap);
+
+
+    
+
+    L.circle(location,{
+      opacity: 0.5,
+      fillOpacity: 0.75,
+      weight: 0.5,
+      color: 'black',
+      fillColor: 'green',
+      radius: 10000
+    }).bindPopup(`<p align = "left"> <strong>City:</strong> ${city} <br>
+    <strong>Temperature:</strong> ${maxtemp} <br> <strong>Humidity:</strong> ${humidity} <br> <strong>Wind Speed: </strong> ${wind} </p>`).addTo(myMap)
+
+    newMarker = L.layer
+  
+ 
 });
