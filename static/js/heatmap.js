6fe2a49562d98d808d184d150d42515317721dd8
nnoark <nnoark@gmail.com>
2021-11-19 14:30:40 -0500

    map updated

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,5 +1,5 @@
 var myMap = L.map("map", {
-    center: [37.7749, -122.4194],
+    center: [44.58, -103.46],
     zoom: 5
   });
 
@@ -8,20 +8,20 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-const data = "static/resources/weather.json"
+const json = "static/resources/weather.json";
 
-d3.json(data).then(function(data) {
+d3.json(json).then(function(data) {
 
   console.log(data);
 
   var heatArray = [];
 
-  for (var i = 0; i < data.length; i++) {
-    var x = [data[i].metadata];
+  for (var i = 0; i < data.metadata.length; i++) {
+    var location = [data.metadata[i].Lat,data.metadata[i].Lng]
   
 
-    if (x) {
-      heatArray.push([data.Lat[i], data.Lng[i]]);
+    if (location) {
+      heatArray.push([location[0],location[1]]);
     }
   }
 
